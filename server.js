7a86df10abfe50a75b9f63c8b37607dd8215e351
MicahBear <micah.b.elm@gmail.com>
2022-07-06 12:59:41 -0700

    update server to work with ejs,test post method

--- server.js
+++ server.js
@@ -3,21 +3,36 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.listen(3000, () => { console.log('listening on 3000') })
-
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-app.post('/equipment', (req, res) => {
-    console.log(req.body)
-})
-
-MongoClient.connect('mongodb+srv://bear:Mdsoccer1@cluster0.77wby1r.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true })
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
+// app.post('/equipment', (req, res) => {
+//     console.log(req.body)
+// })
+const connectionString = 'mongodb+srv://bear:Mdsoccer1@cluster0.77wby1r.mongodb.net/?retryWrites=true&w=majority'
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
         console.log("connected to database")
+        const db = client.db('dnd-inventory')
+        const inventoryCollection = db.collection('equipment')
+        app.set('view engine', 'ejs')
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        app.get('/', (req, res) => {
+            db.collection('equipment').find().toArray()
+                .then(results => {
+                    res.render('index.ejs', { equipment: results })
+                })
+                .catch(error => console.error(error))
+        })
+        app.post('/equipment', (req, res) => {
+            inventoryCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+        app.listen(3000, () => { console.log('listening on 3000') })
     })
     .catch(error => console.log(error))
